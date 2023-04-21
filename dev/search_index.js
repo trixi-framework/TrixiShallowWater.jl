1e0451c692453c146e6318bb96338c2ca99c1c42
Documenter.jl <documenter@juliadocs.github.io>
2023-04-21 18:41:36 +0000

    build based on e2576a0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TrixiShallowWater","category":"page"},{"location":"#TrixiShallowWater","page":"Home","title":"TrixiShallowWater","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TrixiShallowWater.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TrixiShallowWater]","category":"page"}]
+}
