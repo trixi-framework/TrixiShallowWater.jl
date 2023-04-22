97273914ccfba73cf893af3dda139098b8728196
Michael Schlottke-Lakemper <michael@sloede.com>
2023-04-22 06:43:27 +0200

    Add Trixi.jl as a dependency and reexport it (#3)

--- src/TrixiShallowWater.jl
+++ src/TrixiShallowWater.jl
@@ -1,5 +1,11 @@
 module TrixiShallowWater
 
+# import @reexport now to make it available for further imports/exports
+using Reexport: @reexport
+
+# Make all of Trixi.jl available to a user of this package
+@reexport using Trixi
+
 # Write your package code here.
 foo() = true
 bar() = false
