769078fff275d4e0ef6af8c6ce220c3575ea361a
Michael Schlottke-Lakemper <michael@sloede.com>
2023-11-05 12:31:36 +0100

    Add test group for testing from upstream (= Trixi.jl) (#7)
    * Do not reexport Trixi.jl
    
    * Add Trixi as test dependency
    
    * Add function that actually exercises Trixi

--- src/TrixiShallowWater.jl
+++ src/TrixiShallowWater.jl
@@ -1,13 +1,10 @@
 module TrixiShallowWater
 
-# import @reexport now to make it available for further imports/exports
-using Reexport: @reexport
-
-# Make all of Trixi.jl available to a user of this package
-@reexport using Trixi
+using Trixi: Trixi
 
 # Write your package code here.
 foo() = true
 bar() = false
+baz() = Trixi.examples_dir()
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,24 @@
 using TrixiShallowWater
 using Test
 
-@testset "TrixiShallowWater.jl" begin
-  @test TrixiShallowWater.foo() == true
-  @test TrixiShallowWater.bar() == false
+# We run tests in parallel with CI jobs setting the `TRIXI_TEST` environment
+# variable to determine the subset of tests to execute.
+# By default, we just run the threaded tests since they are relatively cheap
+# and test a good amount of different functionality.
+const TRIXI_TEST = get(ENV, "TRIXI_TEST", "all")
+const TRIXI_MPI_NPROCS = clamp(Sys.CPU_THREADS, 2, 3)
+const TRIXI_NTHREADS = clamp(Sys.CPU_THREADS, 2, 3)
+
+@time @testset "TrixiShallowWater.jl tests" begin
+    @time if TRIXI_TEST == "all"
+        @test TrixiShallowWater.foo() == true
+        @test TrixiShallowWater.bar() == false
+        @test TrixiShallowWater.baz() isa String
+    end
+
+    @time if TRIXI_TEST == "all" || TRIXI_TEST == "upstream"
+        @testset "baz()" begin
+            @test TrixiShallowWater.baz() isa String
+        end
+    end
 end
