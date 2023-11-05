fb61df8cab38bea70ee1c25cb60db5c1d2df475a
Michael Schlottke-Lakemper <michael@sloede.com>
2023-11-05 12:03:33 +0100

    Do not reexport Trixi.jl

--- src/TrixiShallowWater.jl
+++ src/TrixiShallowWater.jl
@@ -1,10 +1,6 @@
 module TrixiShallowWater
 
-# import @reexport now to make it available for further imports/exports
-using Reexport: @reexport
-
-# Make all of Trixi.jl available to a user of this package
-@reexport using Trixi
+using Trixi: Trixi
 
 # Write your package code here.
 foo() = true
