c9cb2ea008e98244fc453d6702e11cb5fede831b
patrickersing <patrick.ersing@liu.se>
2023-12-18 09:03:57 +0100

    add equation SWEWetDry1D

--- src/TrixiShallowWater.jl
+++ src/TrixiShallowWater.jl
@@ -1,10 +1,14 @@
 module TrixiShallowWater
-
+# Decide between using Trixi: Trixi, import Trixi or using Trixi?
 using Trixi: Trixi
+using MuladdMacro: @muladd
+using StaticArrays: SVector
+using Static: True, False
 
-# Write your package code here.
-foo() = true
-bar() = false
-baz() = Trixi.examples_dir()
+include("equations/equations.jl")
+
+# export types/functions that define the public API of TrixiShallowWater.jl
+export ShallowWaterEquationsWetDry1D
+export hydrostatic_reconstruction_chen_noelle, flux_nonconservative_chen_noelle
 
 end
---
+++ src/equations/equations.jl
@@ -0,0 +1,13 @@
+# By default, Julia/LLVM does not use fused multiply-add operations (FMAs).
+# Since these FMAs can increase the performance of many numerical algorithms,
+# we need to opt-in explicitly.
+# See https://ranocha.de/blog/Optimizing_EC_Trixi for further details.
+@muladd begin
+#! format: noindent
+
+####################################################################################################
+# Include files with actual implementations for different systems of equations. 
+# Numerical flux formulations that are independent of the specific system of equations
+
+include("shallow_water_wet_dry_1d.jl")
+end # @muladd
