c47527e3d0c602df76a5f202a13754a1f3862765
patrickersing <patrick.ersing@liu.se>
2024-01-22 17:07:32 +0100

    add unit tests

--- test/test_unit.jl
+++ test/test_unit.jl
@@ -14,11 +14,20 @@ isdir(outdir) && rm(outdir, recursive = true)
 @testset "Unit tests" begin
 #! format: noindent
 
+@timed_testset "Printing indicators/controllers" begin
+    # OBS! Constructing indicators/controllers using the parameters below doesn't make sense. It's
+    # just useful to run basic tests of `show` methods.
+
+    indicator_hg_swe = IndicatorHennemannGassnerShallowWater(1.0, 0.0, true, "variable",
+                                                             "cache")
+    @test_nowarn show(stdout, indicator_hg_swe)
+end
+
 @timed_testset "Shallow water conversion between conservative/entropy variables" begin
-    H, v, b = 3.5, 0.25, 0.4
+    H, v1, v2, b = 3.5, 0.25, 0.1, 0.4
 
     let equations = ShallowWaterEquationsWetDry1D(gravity_constant = 9.8)
-        cons_vars = Trixi.prim2cons(SVector(H, v, b), equations)
+        cons_vars = Trixi.prim2cons(SVector(H, v1, b), equations)
         entropy_vars = Trixi.cons2entropy(cons_vars, equations)
         @test cons_vars ≈ Trixi.entropy2cons(entropy_vars, equations)
 
@@ -28,9 +37,23 @@ isdir(outdir) && rm(outdir, recursive = true)
               Trixi.energy_internal(cons_vars, equations) +
               energy_kinetic(cons_vars, equations)
         # test tuple args
-        cons_vars = Trixi.prim2cons((H, v, b), equations)
+        cons_vars = Trixi.prim2cons((H, v1, b), equations)
         entropy_vars = Trixi.cons2entropy(cons_vars, equations)
         @test cons_vars ≈ Trixi.entropy2cons(entropy_vars, equations)
+    end
+
+    let equations = ShallowWaterEquationsWetDry2D(gravity_constant = 9.8)
+        cons_vars = prim2cons(SVector(H, v1, v2, b), equations)
+        entropy_vars = cons2entropy(cons_vars, equations)
+        @test cons_vars ≈ entropy2cons(entropy_vars, equations)
+
+        total_energy = energy_total(cons_vars, equations)
+        @test total_energy ≈ entropy(cons_vars, equations)
+
+        # test tuple args
+        cons_vars = prim2cons((H, v1, v2, b), equations)
+        entropy_vars = cons2entropy(cons_vars, equations)
+        @test cons_vars ≈ entropy2cons(entropy_vars, equations)
     end
 end
 end
