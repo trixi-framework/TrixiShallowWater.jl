6bb65dd8ea9f89aebd350810f23954c30ff144a2
Patrick Ersing <114223904+patrickersing@users.noreply.github.com>
2024-01-31 14:25:58 +0100

    Add spell check and format check workflows (#17)
    * add permissions for cache deletion
    
    * add format check workflow
    
    * add spell check
    
    * fix typo
    
    * apply formatter
    
    * Update .github/workflows/FormatCheck.yml
    
    * apply formatter
    
    ---------
    
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,23 @@
 using TrixiShallowWater
 using Documenter
 
-DocMeta.setdocmeta!(TrixiShallowWater, :DocTestSetup, :(using TrixiShallowWater); recursive=true)
+DocMeta.setdocmeta!(TrixiShallowWater, :DocTestSetup, :(using TrixiShallowWater);
+                    recursive = true)
 
 makedocs(;
-    modules=[TrixiShallowWater],
-    authors="Andrew R. Winters <andrew.ross.winters@liu.se>, Michael Schlottke-Lakemper <michael@sloede.com>",
-    repo="https://github.com/trixi-framework/TrixiShallowWater.jl/blob/{commit}{path}#{line}",
-    sitename="TrixiShallowWater.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://trixi-framework.github.io/TrixiShallowWater.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
-)
+         modules = [TrixiShallowWater],
+         authors = "Andrew R. Winters <andrew.ross.winters@liu.se>, Michael Schlottke-Lakemper <michael@sloede.com>",
+         repo = "https://github.com/trixi-framework/TrixiShallowWater.jl/blob/{commit}{path}#{line}",
+         sitename = "TrixiShallowWater.jl",
+         format = Documenter.HTML(;
+                                  prettyurls = get(ENV, "CI", "false") == "true",
+                                  canonical = "https://trixi-framework.github.io/TrixiShallowWater.jl",
+                                  edit_link = "main",
+                                  assets = String[],),
+         pages = [
+             "Home" => "index.md",
+         ],)
 
 deploydocs(;
-    repo="github.com/trixi-framework/TrixiShallowWater.jl",
-    devbranch="main",
-)
+           repo = "github.com/trixi-framework/TrixiShallowWater.jl",
+           devbranch = "main",)
