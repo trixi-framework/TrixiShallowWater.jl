2c597909020e1d9009397801a2d82c813cf21c4a
Patrick Ersing <114223904+patrickersing@users.noreply.github.com>
2024-04-27 09:54:54 +0200

    adjust comment
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>

--- src/equations/shallow_water_multilayer_1d.jl
+++ src/equations/shallow_water_multilayer_1d.jl
@@ -392,10 +392,10 @@ end
 """
     hydrostatic_reconstruction_ersing_etal(u_ll, u_rr, equations::ShallowWaterMultiLayerEquations1D)
 
-A particular type of hydrostatic reconstruction of the water height and bottom topography to 
+A particular hydrostatic reconstruction of the water height and bottom topography to 
 guarantee well-balancedness in the presence of wet/dry transitions and entropy stability for the 
 [`ShallowWaterMultiLayerEquations1D`](@ref). 
-The reconstructed solution states `u_ll_star` and `u_rr_star` variables are used to evaluate the 
+The reconstructed solution states `u_ll_star` and `u_rr_star` are used to evaluate the 
 surface numerical flux at the interface. The key idea is a piecewise linear reconstruction of the 
 bottom topography and water height interfaces using subcells, where the bottom topography is allowed 
 to be discontinuous. 
