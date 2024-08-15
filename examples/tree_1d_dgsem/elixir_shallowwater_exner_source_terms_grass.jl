cbc57dc13d719485d5256cc1301d74c37179eb5f
Patrick Ersing <114223904+patrickersing@users.noreply.github.com>
2024-08-15 08:13:07 +0200

    Shallow water exner equations 1D (#51)
    * add SWE-Exner1D
    
    * fix docstrings
    
    * add zero velocity workaround to roe flux
    
    * fix typo
    
    * add roots
    
    * fix convergence tests
    
    * fix convergence tests 2
    
    * fix convergence tests 3
    
    * update test_tree_1d reference values
    
    * add tests to increase coverage
    
    * Apply suggestions from code review
    
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>
    
    * Modify docstrings, fix function q_s
    
    ---------
    
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>

---
+++ examples/tree_1d_dgsem/elixir_shallowwater_exner_source_terms_grass.jl
@@ -0,0 +1,69 @@
+using OrdinaryDiffEq
+using Trixi
+using TrixiShallowWater
+
+###############################################################################
+# Semidiscretization of the SWE-Exner equations with source terms for convergence testing
+
+# Equations with Grass model
+equations = ShallowWaterExnerEquations1D(gravity_constant = 10.0, rho_f = 0.5,
+                                         rho_s = 1.0, porosity = 0.5,
+                                         friction = ManningFriction(n = 0.0),
+                                         sediment_model = GrassModel(A_g = 0.01))
+
+initial_condition = initial_condition_convergence_test
+
+###############################################################################
+# Get the DG approximation space
+
+volume_flux = (flux_ersing_etal, flux_nonconservative_ersing_etal)
+surface_flux = (flux_ersing_etal, flux_nonconservative_ersing_etal)
+
+solver = DGSEM(polydeg = 4,
+               surface_flux = surface_flux,
+               volume_integral = VolumeIntegralFluxDifferencing(volume_flux))
+
+###############################################################################
+# Get the TreeMesh and setup a periodic mesh
+
+coordinates_min = 0.0
+coordinates_max = sqrt(2.0)
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level = 2,
+                n_cells_max = 10_000,
+                periodicity = true)
+
+# create the semi discretization object
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
+                                    source_terms = source_terms_convergence_test)
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+tspan = (0.0, 1.0)
+ode = semidiscretize(semi, tspan)
+
+summary_callback = SummaryCallback()
+
+analysis_interval = 200
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
+
+alive_callback = AliveCallback(analysis_interval = analysis_interval)
+
+stepsize_callback = StepsizeCallback(cfl = 1.0)
+
+save_solution = SaveSolutionCallback(interval = 200,
+                                     save_initial_solution = true,
+                                     save_final_solution = true)
+
+callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback,
+                        stepsize_callback, save_solution)
+
+###############################################################################
+# run the simulation
+
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false),
+            dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+            save_everystep = false, callback = callbacks);
+
+summary_callback() # print the timer summary
