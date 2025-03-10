75632c39a911d31c6b2e0490cdcbc73739791c47
Patrick Ersing <114223904+patrickersing@users.noreply.github.com>
2025-03-10 08:54:28 +0100

    Apply suggestions from code review
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>

--- src/equations/shallow_water_wet_dry_1d.jl
+++ src/equations/shallow_water_wet_dry_1d.jl
@@ -495,6 +495,12 @@ end
                                     equations::ShallowWaterEquationsWetDry1D)
 Roe-type dissipation term for the [`ShallowWaterEquationsWetDry1D`](@ref). To create the classical Roe solver,
 this dissipation term can be combined with [`Trixi.flux_central`](@extref) using [`Trixi.FluxPlusDissipation`](@extref).
+
+For details on the Roe linearization see Chapter 15.3.2 and Chapter 15.3.3 for the one-dimensional
+shallow water equations of the book:
+- Randall J. LeVeque (2002)
+  Finite Volume Methods for Hyperbolic Problems
+  [DOI: 10.1017/CBO9780511791253](https://doi.org/10.1017/CBO9780511791253)
 """
 @inline function dissipation_roe(u_ll, u_rr, orientation_or_normal_direction,
                                  equations::ShallowWaterEquationsWetDry1D)
--- src/equations/shallow_water_wet_dry_2d.jl
+++ src/equations/shallow_water_wet_dry_2d.jl
@@ -614,6 +614,12 @@ end
                                     equations::ShallowWaterEquationsWetDry2D)
 Roe-type dissipation term for the [`ShallowWaterEquationsWetDry2D`](@ref). To create the classical Roe solver,
 this dissipation term can be combined with [`Trixi.flux_central`](@extref) using [`Trixi.FluxPlusDissipation`](@extref).
+
+For details on the Roe linearization see Chapter 15.3.2 and Chapter 21.7 for the two-dimensional
+shallow water equations of the book:
+- Randall J. LeVeque (2002)
+  Finite Volume Methods for Hyperbolic Problems
+  [DOI: 10.1017/CBO9780511791253](https://doi.org/10.1017/CBO9780511791253)
 """
 @inline function dissipation_roe(u_ll, u_rr, normal_direction::AbstractVector,
                                  equations::ShallowWaterEquationsWetDry2D)
