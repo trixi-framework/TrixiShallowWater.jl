e354bc75188bb0c04c4d71cfb56240865081ea7f
Andrew Winters <andrew.ross.winters@liu.se>
2025-04-01 21:16:36 +0200

    add tests

---
+++ test/test_p4est_2d.jl
@@ -0,0 +1,130 @@
+module TestExamplesUnstructuredMesh2D
+
+using Test
+using Trixi
+using TrixiShallowWater
+
+include("test_trixi.jl")
+
+EXAMPLES_DIR = pkgdir(TrixiShallowWater, "examples", "p4est_2d_dgsem")
+
+# Start with a clean environment: remove TrixiShallowWater.jl output directory if it exists
+outdir = "out"
+isdir(outdir) && rm(outdir, recursive = true)
+
+@testset "P4estMesh2D" begin
+#! format: noindent
+
+@testset "Shallow Water Wet/Dry" begin
+    @trixi_testset "elixir_shallowwater_well_balanced.jl" begin
+        @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                     "elixir_shallowwater_well_balanced.jl"),
+                            l2=[
+                                0.2018723974651268,
+                                4.798334932564209e-14,
+                                5.265746106509223e-14,
+                                0.20187239746512692
+                            ],
+                            linf=[
+                                0.41600528180178653,
+                                1.4686699343325025e-12,
+                                2.6443321644596183e-12,
+                                0.41600528180178625
+                            ],
+                            tspan=(0.0, 0.25),
+                            atol=1e-10)
+        # Ensure that we do not have excessive memory allocations
+        # (e.g., from type instabilities)
+        let
+            t = sol.t[end]
+            u_ode = sol.u[end]
+            du_ode = similar(u_ode)
+            @test (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 1000
+        end
+    end
+
+    @trixi_testset "elixir_shallowwater_perturbation.jl" begin
+        @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                     "elixir_shallowwater_perturbation.jl"),
+                            l2=[
+                                0.02263230105470324,
+                                0.09090425233020173,
+                                0.09124622065757255,
+                                0.0011045848311422332
+                            ],
+                            linf=[
+                                0.3118823726810007,
+                                0.7855402508435719,
+                                0.7401368273982774,
+                                0.011669083581857587
+                            ],
+                            tspan=(0.0, 0.025))
+        # Ensure that we do not have excessive memory allocations
+        # (e.g., from type instabilities)
+        let
+            t = sol.t[end]
+            u_ode = sol.u[end]
+            du_ode = similar(u_ode)
+            @test (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 1000
+        end
+    end
+
+    @trixi_testset "elixir_shallowwater_well_balanced_wet_dry.jl" begin
+        @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                     "elixir_shallowwater_well_balanced_wet_dry.jl"),
+                            l2=[
+                                0.17389058166483418,
+                                1.624323598540152e-14,
+                                1.7514855451277318e-14,
+                                0.1841504979260916
+                            ],
+                            linf=[
+                                0.4160052818017864,
+                                5.161569959778033e-13,
+                                9.275410298170494e-13,
+                                0.41600528180178625
+                            ],
+                            tspan=(0.0, 0.25),
+                            atol=1e-10)
+        # Ensure that we do not have excessive memory allocations
+        # (e.g., from type instabilities)
+        let
+            t = sol.t[end]
+            u_ode = sol.u[end]
+            du_ode = similar(u_ode)
+            @test (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 1000
+        end
+    end
+
+    @trixi_testset "elixir_shallowwater_perturbation_wet_dry.jl" begin
+        @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                     "elixir_shallowwater_perturbation_wet_dry.jl"),
+                            l2=[
+                                0.41088382134615264,
+                                0.3366854598800536,
+                                0.3926373425478459,
+                                0.45643505721307376
+                            ],
+                            linf=[
+                                2.1242584564331213,
+                                3.381925445720203,
+                                3.4612678671693673,
+                                0.7495177590247986
+                            ],
+                            tspan=(0.0, 0.015))
+        # Ensure that we do not have excessive memory allocations
+        # (e.g., from type instabilities)
+        let
+            t = sol.t[end]
+            u_ode = sol.u[end]
+            du_ode = similar(u_ode)
+            @test (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 1000
+        end
+    end
+end # SWE
+end # P4estMesh2D
+
+# Clean up afterwards: delete TrixiShallowWater.jl output directory
+@test_nowarn rm(outdir, recursive = true)
+
+end # module
