a25a6d76b58598ad8c5400ffc153d94f49d07047
Patrick Ersing <114223904+patrickersing@users.noreply.github.com>
2025-04-09 11:45:42 +0200

    replace Trixi.waterheight and Trixi.waterheight_pressure (#81)
    * replace Trixi.waterheight and Trixi.waterheight_pressure
    
    * switch to waterheight in docs
    
    * update Project.toml files
    
    ---------
    
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>

--- examples/tree_1d_dgsem/elixir_shallowwater_multilayer_well_balanced_wet_dry.jl
+++ examples/tree_1d_dgsem/elixir_shallowwater_multilayer_well_balanced_wet_dry.jl
@@ -157,7 +157,7 @@ save_solution = SaveSolutionCallback(interval = 1000,
                                      save_initial_solution = true,
                                      save_final_solution = true)
 
-stage_limiter! = PositivityPreservingLimiterShallowWater(variables = (Trixi.waterheight,))
+stage_limiter! = PositivityPreservingLimiterShallowWater(variables = (waterheight,))
 
 callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback, save_solution,
                         stepsize_callback)
