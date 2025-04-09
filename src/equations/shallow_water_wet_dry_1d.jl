d1b76c8fcca75a01a70219bf81ed02552a237d27
Patrick Ersing <114223904+patrickersing@users.noreply.github.com>
2025-04-09 11:02:46 +0200

    Update src/equations/shallow_water_wet_dry_1d.jl
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>

--- src/equations/shallow_water_wet_dry_1d.jl
+++ src/equations/shallow_water_wet_dry_1d.jl
@@ -24,7 +24,7 @@ also defines the total water height as ``H = h + b``.
 The additional quantity ``H_0`` is also available to store a reference value for the total water height that
 is useful to set initial conditions or test the "lake-at-rest" well-balancedness.
 
-Also, there are four thresholds which prevent numerical problems as well as instabilities. None of them have
+Also, there are four thresholds which prevent numerical problems as well as instabilities. None of them
 have to be passed, as default values are defined within the struct. The first one, `threshold_limiter`, is
 used in [`PositivityPreservingLimiterShallowWater`](@ref) on the water height, as a (small) shift on the initial
 condition and cutoff before the next time step. The second one, `threshold_wet`, is applied on the water height to
