f799ac6d4124bbb30a64c483fb851ab63c7120b2
Andrew Winters <andrew.ross.winters@liu.se>
2025-05-07 20:30:49 +0200

    attempt to add MPI tests to CI

---
+++ test/test_mpi.jl
@@ -0,0 +1,39 @@
+module TestExamplesMPI
+
+using Test
+using Trixi
+using TrixiShallowWater
+
+include("test_trixi.jl")
+
+# Start with a clean environment: remove Trixi.jl output directory if it exists
+outdir = "out"
+Trixi.mpi_isroot() && isdir(outdir) && rm(outdir, recursive = true)
+Trixi.MPI.Barrier(Trixi.mpi_comm())
+
+@testset "MPI" begin
+    # TreeMesh tests
+    include("test_mpi_tree.jl")
+
+    # P4estMesh and T8codeMesh tests
+    include("test_mpi_p4est_2d.jl")
+end # MPI
+
+@trixi_testset "MPI supporting functionality" begin
+    t = 0.5
+    let u = 1.0
+        @test ode_norm(u, t) ≈ Trixi.DiffEqBase.ODE_DEFAULT_NORM(u, t)
+    end
+    let u = [1.0, -2.0]
+        @test ode_norm(u, t) ≈ Trixi.DiffEqBase.ODE_DEFAULT_NORM(u, t)
+    end
+    let u = [SVector(1.0, -2.0), SVector(0.5, -0.1)]
+        @test ode_norm(u, t) ≈ Trixi.DiffEqBase.ODE_DEFAULT_NORM(u, t)
+    end
+end # MPI supporting functionality
+
+# Clean up afterwards: delete Trixi.jl output directory
+Trixi.mpi_isroot() && @test_nowarn rm(outdir, recursive = true)
+Trixi.MPI.Barrier(Trixi.mpi_comm())
+
+end # module
