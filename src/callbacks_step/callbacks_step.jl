a57b21a936cac7f58342d1e39b55418e5fee071f
Andrew Winters <andrew.ross.winters@liu.se>
2025-05-07 12:31:00 +0200

    add limiter calls in the refine and coarsen of AMR callback

---
+++ src/callbacks_step/callbacks_step.jl
@@ -0,0 +1,9 @@
+# By default, Julia/LLVM does not use fused multiply-add operations (FMAs).
+# Since these FMAs can increase the performance of many numerical algorithms,
+# we need to opt-in explicitly.
+# See https://ranocha.de/blog/Optimizing_EC_Trixi for further details.
+@muladd begin
+#! format: noindent
+
+include("amr_dg2d.jl")
+end # @muladd
