8666001b1d68ea2d2dddd0a2740d0cd734c2d45a
patrickersing <patrick.ersing@liu.se>
2026-03-08 11:17:56 +0100

    use local normal direction for flux_ersing_local_jump

--- src/equations/shallow_water_multilayer_2d.jl
+++ src/equations/shallow_water_multilayer_2d.jl
@@ -714,7 +714,7 @@ end
         f_h = zero(real(equations))
         f_hv = g * h_ll[i]
 
-        setlayer!(f, f_h, f_hv, f_hv, i, equations)
+        setlayer!(f, f_h, f_hv * normal_direction[1], f_hv * normal_direction[2], i, equations)
     end
 
     return SVector(f)
@@ -812,8 +812,8 @@ end
                 f_hv += h_jump[j]
             end
         end
-        setlayer!(f, f_h, f_hv * normal_direction[1],
-                  f_hv * normal_direction[2], i, equations)
+        setlayer!(f, f_h, f_hv,
+                  f_hv, i, equations)
     end
 
     return SVector(f)
