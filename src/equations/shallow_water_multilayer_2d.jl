2f3484f1e0cb9ddd9aab4ade3d4b98a36769e306
patrickersing <patrick.ersing@liu.se>
2026-06-18 14:30:09 +0200

    update arguments for nonconservative subcell fluxes

--- src/equations/shallow_water_multilayer_2d.jl
+++ src/equations/shallow_water_multilayer_2d.jl
@@ -789,7 +789,8 @@ Jump part of the nonconservative term needed for the calculation of the non-cons
 end
 
 @inline function flux_nonconservative_ersing_etal_local_jump(u_ll, u_rr,
-                                                             normal_direction::AbstractVector,
+                                                             normal_direction_ll::AbstractVector,
+                                                             normal_direction_rr::AbstractVector,
                                                              equations::ShallowWaterMultiLayerEquations2D,
                                                              nonconservative_type::Trixi.NonConservativeJump,
                                                              nonconservative_term::Integer)
